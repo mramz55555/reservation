963dc9ab3e603cb281898928b188a970d8c7b8b7
mramz55555 <mramz55555@gmail.com>
2023-02-18 21:37:20 +0330

    commit2

--- src/main/java/com/isoft/reservation/ReservationApplication.java
+++ src/main/java/com/isoft/reservation/ReservationApplication.java
@@ -3,18 +3,10 @@ package com.isoft.reservation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalTime;
-
 @SpringBootApplication
 public class ReservationApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReservationApplication.class, args);
     }
 
-}
-
-class Main {
-    public static void main(String[] args) {
-        System.out.println(LocalTime.parse("13:00"));
-    }
 }
